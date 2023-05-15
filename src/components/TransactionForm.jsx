5d35a2f329510eb17367dab040804e97f8a458e7
nATOr <gowluonade@gmail.com>
2023-05-15 00:05:36 -0500

    form

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -1,6 +1,29 @@
+import { useState } from "react"
+
 function TransactionForm() {
+  const [description, setDescription] = useState(0)
+  const [amount, setAmount] = useState(0)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(description,amount)
+  }
   return (
-    <div>TransactionForm</div>
+    <section>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="Description" 
+          onChange={(e)=>setDescription(e.target.value)} 
+        />
+        <input 
+          type="number" 
+          placeholder="00.00" 
+          step={0.01}
+          onChange={(e)=>setAmount(e.target.value)} 
+        />
+        <button>Add Transaction</button>
+      </form>
+    </section>
   )
 }
 export default TransactionForm
