4fcf210afa101a0ada9b450fbe60a4f4d8984c01
nATOr <gowluonade@gmail.com>
2023-05-15 02:20:33 -0500

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Balance from "./components/Balance"
+import ExpenseChart from "./components/ExpenseChart"
 import Header from "./components/Header"
 import IncomeExpenses from "./components/IncomeExpenses"
 import TransactionForm from "./components/transactions/TransactionForm"
@@ -9,14 +10,19 @@ function App() {
   return (
     <GlobalProvider>
       <main className="h-screen flex justify-center items-center bg-zinc-950">
-        <div className="bg-zinc-800 p-10 rounded-lg flex gap-x-10 container w-3/6 mx-auto">
-          <section className="grid gap-y-1 w-4/6 my-5">
-            <Header/>
-            <IncomeExpenses/>
-            <Balance/>
-            <TransactionForm/>
+        <div className="bg-zinc-800 p-10 rounded-lg flex gap-x-10 container w-4/6 mx-auto">
+          <div className="grid">
+            <section className="grid gap-y-1 my-5 max-h-[300px] max-w-[300px]">
+              <Header/>
+              <IncomeExpenses/>
+              <Balance/>
+              <TransactionForm/>
+              <ExpenseChart/>
+            </section>
+          </div>
+          <section className="flex-1">
+            <TransactionList/>
           </section>
-          <TransactionList/>
         </div>
       </main>
     </GlobalProvider>
